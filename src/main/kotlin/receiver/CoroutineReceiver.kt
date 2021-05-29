863fae4925ead714de7425730baef39550e53961
NyCode <nico@nycode.de>
2021-05-29 18:05:38 +0200

    Add consuming member functions

--- src/main/kotlin/receiver/CoroutineReceiver.kt
+++ src/main/kotlin/receiver/CoroutineReceiver.kt
@@ -17,8 +17,40 @@
 
 package de.nycode.rabbitkt.receiver
 
+import com.rabbitmq.client.Delivery
+import de.nycode.rabbitkt.queue.Queue
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.reactive.asFlow
+import reactor.rabbitmq.AcknowledgableDelivery
 import reactor.rabbitmq.Receiver
 import java.io.Closeable
+
+@JvmInline
+public value class AcknowledgeHandler internal constructor(
+    private val acknowledgableDelivery: AcknowledgableDelivery
+) {
+
+    /**
+     * The underlying delivery of this handler.
+     */
+    public val delivery: Delivery
+        get() = acknowledgableDelivery
+
+    /**
+     * Acknowledges the delivery.
+     * @param multiple acknowledge multiple messages or not
+     */
+    public fun ack(multiple: Boolean = false): Unit = acknowledgableDelivery.ack(multiple)
+
+    /**
+     * Rejects the delivery.
+     * @param multiple acknowledge multiple messages or not.
+     * @param requeue requeue the message into the broker or not.
+     */
+    public fun reject(multiple: Boolean = false, requeue: Boolean): Unit =
+        acknowledgableDelivery.nack(multiple, requeue)
+}
 
 @JvmInline
 public value class CoroutineReceiver(private val receiver: Receiver) : Closeable {
@@ -26,4 +58,49 @@ public value class CoroutineReceiver(private val receiver: Receiver) : Closeable
      * Closes the underlying [Receiver].
      */
     override fun close(): Unit = receiver.close()
+
+    /**
+     * Consume messages of the given [queue] with a [Flow].
+     * Every message gets acknowledged automatically.
+     * @param queue the targeted queue.
+     * @return A [Flow] of deliveries.
+     */
+    public fun consumeAutoAckFlow(queue: Queue): Flow<Delivery> {
+        return receiver.consumeAutoAck(queue.name).asFlow()
+    }
+
+    /**
+     * Consume messages of the given [queue] with your given [handler].
+     * Every message gets acknowledged automatically.
+     * @param queue the targeted queue.
+     * @param handler the handler which gets called for every message.
+     */
+    public suspend fun consumeAutoAck(queue: Queue, handler: suspend (Delivery) -> Unit) {
+        receiver.consumeAutoAck(queue.name).asFlow().collect(handler)
+    }
+
+    /**
+     * Consume messages of the given [queue] with a [Flow].
+     * You have to acknowledge every message manually with either
+     * [AcknowledgeHandler.ack] or [AcknowledgeHandler.reject].
+     * @param queue the targeted queue.
+     * @return A [Flow] of deliveries which must be acknowledged or rejected.
+     */
+    public fun consume(queue: Queue): Flow<AcknowledgableDelivery> =
+        receiver.consumeManualAck(queue.name)
+            .asFlow()
+
+    /**
+     * Consume message of the given [queue] with your given [handler].
+     * You have to acknowledge every message manually with either
+     * [AcknowledgeHandler.ack] or [AcknowledgeHandler.reject].
+     * @param queue the targeted queue.
+     * @param handler the handler which gets called for every message.
+     */
+    public suspend fun consume(queue: Queue, handler: suspend AcknowledgeHandler.() -> Unit): Unit =
+        receiver.consumeManualAck(queue.name)
+            .asFlow()
+            .collect {
+                handler(AcknowledgeHandler(it))
+            }
 }
